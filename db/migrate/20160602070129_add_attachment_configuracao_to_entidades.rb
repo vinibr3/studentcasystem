35a45ba9c7f96dfd77a4c49be88a9ed8f4844fb1
vinibr3 <vinicius.deoliveira@outlook.com>
2016-07-10 03:39:03 -0300

    Update 20160602070129_add_attachment_configuracao_to_entidades.rb

--- db/migrate/20160602070129_add_attachment_configuracao_to_entidades.rb
+++ db/migrate/20160602070129_add_attachment_configuracao_to_entidades.rb
@@ -1,11 +1,11 @@
 class AddAttachmentConfiguracaoToEntidades < ActiveRecord::Migration
   def self.up
     change_table :entidades do |t|
-      t.attachment :configuracao
+      t.attachment :logo
     end
   end
 
   def self.down
-    remove_attachment :entidades, :configuracao
+    remove_attachment :entidades, :logo
   end
 end
