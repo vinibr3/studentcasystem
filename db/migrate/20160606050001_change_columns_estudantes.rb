3df24b9f3c14a2298132705741d311bc421dfa5d
vinibr3 <vinicius.deoliveira@outlook.com>
2016-07-10 02:39:03 -0300

    Update 20160606050001_change_columns_estudantes.rb

--- db/migrate/20160606050001_change_columns_estudantes.rb
+++ db/migrate/20160606050001_change_columns_estudantes.rb
@@ -1,6 +1,5 @@
 class ChangeColumnsEstudantes < ActiveRecord::Migration
   def change
-  	rename_column :estudantes, :instituicao_ensino, :instituicao_ensino_id
   	remove_column :estudantes, :cidade_inst_ensino
   	remove_column :estudantes, :uf_inst_ensino
   end
