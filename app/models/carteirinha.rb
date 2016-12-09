4de7bfa8c276ff300d2cc10cbe29f0c53ef23afa
vinicus <vinicius.deoliveira@outlook.com>
2016-12-09 15:15:27 -0200

    Implementada requisição ajax para select de status versao impressa

--- app/models/carteirinha.rb
+++ app/models/carteirinha.rb
@@ -146,7 +146,7 @@ class Carteirinha < ActiveRecord::Base
 	def check_status_carteirinha_apartir_status_pagamento
 		status_carteirinha = Carteirinha.show_status_carteirinha_apartir_do_status_pagamento self.status_pagamento
 		st=false
-		status_carteirinha.each do |key|
+		status_carteirinha.each_key do |key|
 			st = true if self.status_versao_impressa.to_sym == key
 		end
 		errors.add(:status_versao_impressa, "valor inválido para dado Status de Pagamento: #{self.status_pagamento.humanize} ") unless st
