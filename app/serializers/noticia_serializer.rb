dd32081a71c910a841ffa05c7e3989102b9832c1
vinicus <vinicius.deoliveira@outlook.com>
2016-12-29 10:50:07 -0200

    Alterado NoticiasSerializer

--- app/serializers/noticia_serializer.rb
+++ app/serializers/noticia_serializer.rb
@@ -1,10 +1,14 @@
 class NoticiaSerializer < ActiveModel::Serializer
 	ActiveModel::Serializer.root = false
 
-	attributes :type, :id, :title, :author, :photo, :body
+	attributes :type, :id, :titulo, :autor, :foto_url, :body
 
 	def type
 		'noticia'
+	end
+
+	def foto_url
+		object.foto.url
 	end
 
 end
