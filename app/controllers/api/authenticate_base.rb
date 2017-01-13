5403c3de4152d93f74f83d922a404cfa1e0dd11c
vinicus <vinicius.deoliveira@outlook.com>
2017-01-13 20:29:42 -0200

    Alterado RegistrationController

--- app/controllers/api/authenticate_base.rb
+++ app/controllers/api/authenticate_base.rb
@@ -3,24 +3,25 @@ class Api::AuthenticateBase < ApplicationController
 	respond_to :json
 
 	before_filter :cors_preflight_check
-  after_filter :cors_set_access_control_headers
+  	after_filter :cors_set_access_control_headers
 
 	def cors_set_access_control_headers
 	   headers['Access-Control-Allow-Origin'] = '*'
 	   headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
 	   headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
 	   headers['Access-Control-Max-Age'] = "1728000"
-  end
+  	end
 
-  def cors_preflight_check
-    if request.method == 'OPTIONS'
-     headers['Access-Control-Allow-Origin'] = '*'
-     headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
-     headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
-     headers['Access-Control-Max-Age'] = '1728000'
+  	def cors_preflight_check
+	    if request.method == 'OPTIONS'
+		     headers['Access-Control-Allow-Origin'] = '*'
+		     headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+		     headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
+		     headers['Access-Control-Max-Age'] = '1728000'
 
-     render :text => '', :content_type => 'text/plain'
-   end
+		     render :text => '', :content_type => 'text/plain'
+	 	end
+   	end
 
 
 	protected 
