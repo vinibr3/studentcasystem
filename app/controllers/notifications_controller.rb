5e9ad70133c2bd1215466c921796de139c44198a
vinicus <vinicius.deoliveira@outlook.com>
2017-01-17 20:59:13 -0200

    Alterado NotificationsController

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -3,7 +3,7 @@ class NotificationsController < ApplicationController
   before_filter :set_cors_headers
 
   def create
-    @transaction = PagSeguro::Transaction.find_by_code(notifications_params[:notificationCode])
+    @transaction = PagSeguro::Transaction.find_by_code(params[:notificationCode])
 
     if @transaction.errors.empty?
       @estudante = Estudante.find(@transaction.reference)
