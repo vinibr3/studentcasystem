f21fa018650a8577d483be4087a30cbaf77aa430
vinicus <vinicius.deoliveira@outlook.com>
2017-01-19 01:14:18 -0200

    Inserido processamento em background para notificações do pagseguro

--- Gemfile
+++ Gemfile
@@ -55,7 +55,7 @@ gem 'omniauth-facebook'
 # Gerencia variaveis de ambiente
 gem 'figaro', '~> 1.1', '>= 1.1.1'
 # Edita imagens
-#gem 'rmagick', '~> 2.15', '>= 2.15.4'
+gem 'rmagick', '~> 2.15', '>= 2.15.4'
 # Verifica certificados ou fornece um quando não há
 #gem 'certified', '~> 1.0'
 # Gera arquivo .zip
@@ -73,9 +73,11 @@ gem 'activeadmin-wysihtml5', github: 'stefanoverna/activeadmin-wysihtml5'
 gem 'aws-sdk', '< 2.0' # Erro uninitialized constant Paperclip::Storage::S3:AWS for '~> 2.0'
 # Datetimepicker para active admin
 gem 'active_admin_datetimepicker', '~> 0.3.0'
+# Processamento em background
+gem 'sidekiq', '~> 4.2', '>= 4.2.9'
 
 group :production do
- #gem 'unicorn'
+ gem 'unicorn'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,46 +1,51 @@
 class NotificationsController < ApplicationController
+  include Sidekiq::Worker
+
   skip_before_filter :verify_authenticity_token, only: :create
   before_filter :set_cors_headers
 
   def create
     transaction = PagSeguro::Transaction.find_by_notification_code(params[:notificationCode])
-
     if transaction.errors.empty?
       @estudante = Estudante.find(transaction.reference)
-        cart = Carteirinha.where(transaction_id: transaction.code.to_s).first_or_create! do |c|
-          c.nome = @estudante.nome                                             
-          c.rg = @estudante.rg
-          c.cpf = @estudante.cpf
-          c.data_nascimento = @estudante.data_nascimento
-          c.matricula = @estudante.matricula
-          c.expedidor_rg = @estudante.expedidor_rg
-          c.uf_expedidor_rg = @estudante.uf_expedidor_rg
-          @instituicao = @estudante.instituicao_ensino
-          c.instituicao_ensino = @instituicao.nome
-          c.cidade_inst_ensino = @instituicao.cidade.nome
-          c.escolaridade = @estudante.escolaridade.nome
-          c.uf_inst_ensino = @instituicao.estado.sigla
-          c.curso_serie = @estudante.curso.nome
-          c.foto = @estudante.foto
-          c.xerox_rg = @estudante.xerox_rg
-          c.xerox_cpf = @estudante.xerox_cpf
-          c.comprovante_matricula = @estudante.comprovante_matricula
-          c.status_versao_impressa = :pagamento
-          c.layout_carteirinha = @estudante.entidade.layout_carteirinhas.last if @estudante.entidade.layout_carteirinhas
-          c.estudante_id = @estudante.id
-          c.transaction_id = transaction.code
-          c.valor = transaction.gross_amount.to_f
-          c.set_forma_pagamento_by_type(transaction.payment_method.type_id)
-          c.set_status_pagamento_by_code(transaction.status.id)
-        end
-        if cart.status_pagamento_to_i <= 2 && transaction.status.id == "3" # status avançou para 'pago'
-            statuses = Carteirinha.status_versao_impressas.map{|k,v|}
-            cart.status_versao_impressa = statuses[1] # muda status para 'Documentação'
-        end
-        cart.set_status_pagamento_by_code(transaction.status.id)
-        cart.save
+      perform_async(transaction, @estudante)
     end
     render nothing: true, status: 200
+  end
+
+  def perform transaction, estudante
+    cart = Carteirinha.where(transaction_id: transaction.code.to_s).first_or_create! do |c|
+      c.nome = estudante.nome                                             
+      c.rg = @estudante.rg
+      c.cpf = @estudante.cpf
+      c.data_nascimento = @estudante.data_nascimento
+      c.matricula = @estudante.matricula
+      c.expedidor_rg = @estudante.expedidor_rg
+      c.uf_expedidor_rg = @estudante.uf_expedidor_rg
+      @instituicao = @estudante.instituicao_ensino
+      c.instituicao_ensino = @instituicao.nome
+      c.cidade_inst_ensino = @instituicao.cidade.nome
+      c.escolaridade = @estudante.escolaridade.nome
+      c.uf_inst_ensino = @instituicao.estado.sigla
+      c.curso_serie = @estudante.curso.nome
+      c.foto = @estudante.foto
+      c.xerox_rg = @estudante.xerox_rg
+      c.xerox_cpf = @estudante.xerox_cpf
+      c.comprovante_matricula = @estudante.comprovante_matricula
+      c.status_versao_impressa = :pagamento
+      c.layout_carteirinha = @estudante.entidade.layout_carteirinhas.last if @estudante.entidade.layout_carteirinhas
+      c.estudante_id = @estudante.id
+      c.transaction_id = transaction.code
+      c.valor = transaction.gross_amount.to_f
+      c.set_forma_pagamento_by_type(transaction.payment_method.type_id)
+      c.set_status_pagamento_by_code(transaction.status.id)
+    end
+    if cart.status_pagamento_to_i <= 2 && transaction.status.id == "3" # status avançou para 'pago'
+      statuses = Carteirinha.status_versao_impressas.map{|k,v|}
+      cart.status_versao_impressa = statuses[1] # muda status para 'Documentação'
+    end
+    cart.set_status_pagamento_by_code(transaction.status.id)
+    cart.save
   end
 
   private
@@ -50,10 +55,6 @@ class NotificationsController < ApplicationController
 
     def set_cors_headers
       headers['Access-Control-Allow-Origin'] = 'https://sandbox.pagseguro.uol.com.br'
-    end
-
-    def index_in_bounds index, size
-      index >= 0 && index < size
     end
 
 end
